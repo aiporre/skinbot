df494ac02b9ea3e1f97044a7810b6dde6100e019
aiporre <ariel.iporre.rivas@gmail.com>
2023-01-18 12:43:43 +0100

    implemented U-Net

--- skinbot/segmentation.py
+++ skinbot/segmentation.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torch import cat
 
 
 class ConvBlock(nn.Module):
@@ -44,20 +45,60 @@ class UpSample(nn.Module):
     def forward(self, x):
         return self.up_sample(x)
 
-class UpSampleConv:
+class UpSampleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
+        super(UpSampleConv, self).__init__()
         self.up_sample = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
         self.conv = ConvBlock(in_channels, out_channels)
 
-    def forward(self, x):
+    def forward(self, x, x_skip):
         x = self.up_sample(x)
+        x = cat([x_skip, x], dim=0)
         return self.conv(x)
 
-class UpSampleInterpolation:
+class UpSampleInterpolation(nn.Module):
     def __init__(self, in_channels, out_channels):
+        super(UpSampleInterpolation, self).__init__()
         self.up_sample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
-        self.conv = ConvBlock(in_channels, out_channels)
+        self.conv = ConvBlock(in_channels, out_channels, mid_channels=in_channels//2)
+
+    def forward(self, x, x_skip):
+        x = self.up_sample(x)
+        x = cat([x_skip, x], dim=0)
+        return self.conv(x)
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, num_classes, learnable_upsample=True):
+        self.first = ConvBlock(in_channels, 64)
+        # downsample plath
+        self.down_1 = DownSample(64, 128)
+        self.down_2 = DownSample(128, 256)
+        self.down_3 = DownSample(256, 512)
+        # upsample path
+        if learnable_upsample:
+            self.down_4 = DownSample(512, 1024)
+            self.up_1 = UpSampleConv(1024, 512)
+            self.up_2 = UpSampleConv(512, 256)
+            self.up_3 = UpSampleConv(256, 128)
+            self.up_4 = UpSampleConv(128, 64)
+        else:
+            self.down_4 = DownSample(512, 512) # concat +512
+            self.up_1 = UpSampleInterpolation(1024, 256) # concat +256
+            self.up_2 = UpSampleInterpolation(512, 128) # concat +128
+            self.up_3 = UpSampleInterpolation(256, 64) # concat +64
+            self.up_4 = UpSampleInterpolation(128, 64) # concat +
+        self.outc = ConvBlock(64, num_classes)
 
     def forward(self, x):
-        x = self.up_sample(x)
-        return self.conv(x)
+        x1 = self.first(x)
+        d1 = self.down_1(x)
+        d2 = self.down_2(x)
+        d3 = self.down_3(x)
+        d4 = self.down_4(x)
+        x = self.up_1(d4, d3)
+        x = self.up_2(x, d2)
+        x = self.up_3(x, d1)
+        x = self.up_4(x, x1)
+        x = self.outc(x)
+        return x
+
