63d565d7a608aa62b24336564999311d824eff5b
aiporre <ariel.iporre.rivas@gmail.com>
2023-02-19 21:50:55 +0100

    imlelent floacl loss module in logse

--- skinbot/losses.py
+++ skinbot/losses.py
@@ -36,3 +36,22 @@ class MulticlassLoss(torch.nn.Module):
         #     print('x(prob)', x[0])
         # return torch.sum(torch.sum(-torch.ceil(y) * torch.log(x), dim=1))
         return torch.mean(torch.sum(-torch.ceil(y) * torch.log(x) - torch.floor(1 - y) * torch.log(1 - x), dim=1))
+
+
+class FocalLoss(torch.nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        # nn.CrossEntropyLoss()
+        BCE_loss = torch.nn.CrossEntropyLoss(reduce=False)(inputs, targets)
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
