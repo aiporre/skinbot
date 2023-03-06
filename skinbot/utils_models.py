23a89996be3da1d5a19cce11ea5c974fe1091b9d
aiporre <ariel.iporre.rivas@gmail.com>
2023-03-06 14:48:27 +0100

    small cnn confugurable num of convs

--- skinbot/utils_models.py
+++ skinbot/utils_models.py
@@ -2,7 +2,10 @@ import torch
 from torch import nn as nn
 from torchvision import models
 import skinbot.skinlogging as logging
+from skinbot.config import Config
+import math
 
+C = Config()
 def get_mlp(num_inputs, num_outputs, layers=None, dropout=0.5):
     layers = [1024] if layers is None else layers
     instances = []
@@ -15,24 +18,57 @@ def get_mlp(num_inputs, num_outputs, layers=None, dropout=0.5):
     instances.append(nn.Linear(num_inputs, num_outputs))
     return nn.Sequential(*instances)
 
+def get_output_size(model, input_size):
+    B = 1 # batch_size
+    if isinstance(input_size, int):
+        input_size = (B , 3, input_size, input_size)
+    x = torch.randn(input_size)
+    y = model(x)
+    return tuple(y.shape)
+
 class SmallCNN(nn.Module):
     # four layers convolutiona network with input 224x224x3
     def __init__(self, num_classes=2):
         super(SmallCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3, padding='same')
-        self.conv2 = nn.Conv2d(16, 32, 3, padding='same')
-        self.conv3 = nn.Conv2d(32, 64, 3, padding='same')
-        self.conv4 = nn.Conv2d(64, 128, 3, padding='same')
-        self.pool = nn.MaxPool2d(2, 2)
-        self.num_middle = 128 * 14 * 14
-        self.fc = get_mlp(self.num_middle, num_classes, layers=[1024], dropout=0.5)
+        # self.conv1 = nn.Conv2d(3, 16, 3, padding='same')
+        # self.conv2 = nn.Conv2d(16, 32, 3, padding='same')
+        # self.conv3 = nn.Conv2d(32, 64, 3, padding='same')
+        # self.conv4 = nn.Conv2d(64, 128, 3, padding='same')
+        conv_layers_dims = eval(C.config['MODELS']['conv_layers'])
+        input_size = eval(C.config['MODELS']['input_size'])
+        if isinstance(input_size, tuple):
+            in_channels = input_size[-1]
+        else:
+            in_channels = 3
+        modules = []
+        for conv_dim in conv_layers_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Conv2d(in_channels, out_channels=conv_dim,
+                              kernel_size=3, stride=1, padding=1),
+                    nn.BatchNorm2d(conv_dim),
+                    nn.LeakyReLU(),
+                    nn.MaxPool2d(2, 2))
+            )
+            in_channels = conv_dim
+
+        self.backbone = nn.Sequential(*modules)
+
+        self.num_middle = math.prod(get_output_size(self.backbone, input_size))
+
+        fc_layers_dims = eval(C.config['MODELS']['fc_layers'])
+        if len(fc_layers_dims) > 0:
+            self.fc = get_mlp(self.num_middle, num_classes, layers=fc_layers_dims, dropout=0.5)
+        else:
+            self.fc = PlainLayer()
+
 
     def forward(self, x):
-        x = self.pool(nn.ReLU()(self.conv1(x)))
-        x = self.pool(nn.ReLU()(self.conv2(x)))
-        x = self.pool(nn.ReLU()(self.conv3(x)))
-        x = self.pool(nn.ReLU()(self.conv4(x)))
-        x = torch.flatten(x, 1)
+        x = self.backbone(x)
+        if self.flatten:
+            x = torch.flatten(x, 1)
+        else:
+            x = nn.functional.adaptive_max_pool2d(x, output_size=1)
         x = self.fc(x)
         return x
 class PlainLayer(nn.Module):
