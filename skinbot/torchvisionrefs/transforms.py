737e7aa836a6b2218746b162f305f5b67b8c8842
aiporre <ariel.iporre.rivas@gmail.com>
2023-05-24 09:45:15 +0200

    implemented fixed Scale tranformation  for detection

--- skinbot/torchvisionrefs/transforms.py
+++ skinbot/torchvisionrefs/transforms.py
@@ -276,6 +276,88 @@ class RandomPhotometricDistort(nn.Module):
         return image, target
 
 
+class Scale(nn.Module):
+    """Randomly resizes the image and its bounding boxes  within the specified scale range.
+    The class implements the Scale Jitter augmentation as described in the paper
+    `"Simple Copy-Paste is a Strong Data Augmentation Method for Instance Segmentation" <https://arxiv.org/abs/2012.07177>`_.
+
+    Args:
+        target_size (tuple of ints): The target size for the transform provided in (height, weight) format.
+        scale_range (tuple of ints): scaling factor interval, e.g (a, b), then scale is randomly sampled from the
+            range a <= scale <= b.
+        interpolation (InterpolationMode): Desired interpolation enum defined by
+            :class:`torchvision.transforms.InterpolationMode`. Default is ``InterpolationMode.BILINEAR``.
+    """
+
+    def __init__(
+            self,
+            target_size: Tuple[int, int] = (-1, -1),
+            target_scale: Tuple[float, float] = (-1, -1),
+            interpolation: InterpolationMode = InterpolationMode.BILINEAR,
+    ):
+        super().__init__()
+        if target_scale[0] == -1 or target_scale[1] == -1:
+            target_scale = None
+        if target_size[0] == -1 or target_size[1] == -1:
+            target_size = None
+        if target_scale is None and target_size is None:
+            raise ValueError("At least one of target_scale or target_size should be specified.")
+        if target_size is not None:
+            assert target_size[0] > 0 and target_size[1] > 0, "target size must be positive."
+            target_scale = None
+        else:
+            assert target_scale[0] > 0 and target_scale[0] <= target_scale[1], "target scale must be positive."
+            target_size = None
+
+        self.target_size = target_size
+        self.target_scale = target_scale
+        self.interpolation = interpolation
+
+    def forward(
+            self, image: Tensor, target: Optional[Dict[str, Tensor]] = None
+    ) -> Tuple[Tensor, Optional[Dict[str, Tensor]]]:
+        if isinstance(image, torch.Tensor):
+            if image.ndimension() not in {2, 3}:
+                raise ValueError(f"image should be 2/3 dimensional. Got {image.ndimension()} dimensions.")
+            elif image.ndimension() == 2:
+                image = image.unsqueeze(0)
+
+        _, orig_height, orig_width = F.get_dimensions(image)
+        if self.target_size is not None:
+            # r = min(self.target_size[1] / orig_height, self.target_size[0] / orig_width)
+            new_width = self.target_scale[0]  # int(orig_width * r)
+            new_height = self.target_scale[1]  # int(orig_height * r)
+
+            image = F.resize(image, [new_height, new_width], interpolation=self.interpolation)
+
+            if target is not None:
+                target["boxes"][:, 0::2] *= new_width / orig_width
+                target["boxes"][:, 1::2] *= new_height / orig_height
+                if "masks" in target:
+                    target["masks"] = F.resize(
+                        target["masks"], [new_height, new_width], interpolation=InterpolationMode.NEAREST
+                    )
+
+            return image, target
+        else:
+            #scale = self.scale_range[0] + torch.rand(1) * (self.scale_range[1] - self.scale_range[0])
+            #r = min(self.target_size[1] / orig_height, self.target_size[0] / orig_width) * scale
+            r_w = self.target_scale[0]
+            r_h = self.target_scale[1]
+            new_width = int(orig_width * r_w)
+            new_height = int(orig_height * r_h)
+
+            image = F.resize(image, [new_height, new_width], interpolation=self.interpolation)
+
+            if target is not None:
+                target["boxes"][:, 0::2] *= new_width / orig_width
+                target["boxes"][:, 1::2] *= new_height / orig_height
+                if "masks" in target:
+                    target["masks"] = F.resize(
+                        target["masks"], [new_height, new_width], interpolation=InterpolationMode.NEAREST
+                    )
+
+            return image, target
 class ScaleJitter(nn.Module):
     """Randomly resizes the image and its bounding boxes  within the specified scale range.
     The class implements the Scale Jitter augmentation as described in the paper
